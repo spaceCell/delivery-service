47a03404ad7240364aef2a54699f5130e61a1bdc
roadthelife <kirill.space.cell@tuta.io>
2026-04-19 21:51:48 +0500

    chore(config): enable flyway, resilience4j and openapi setup
    Configure runtime and build to support circuit breaker annotations, Swagger UI, Flyway migrations and delivery-service logging defaults.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	checkstyle
 	id("org.springframework.boot") version "3.5.13"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -17,9 +18,16 @@ repositories {
 	mavenCentral()
 }
 
+checkstyle {
+	configFile = file("checkstyle/checkstyle.xml")
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core")
+	implementation("io.github.resilience4j:resilience4j-spring-boot3:2.4.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
